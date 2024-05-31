27867c74db792f1d421730ce9367260470935d3d
BannedForN <bukreev04@mail.ru>
2024-05-31 12:08:38 +0300

    Initial commit

---
+++ DLLMain/DLLMain.cpp
@@ -0,0 +1,39 @@
+﻿#include <windows.h>
+#include <string>
+#include <sstream>
+#include <set>
+#include <algorithm>
+#include <cctype>
+using namespace std;
+
+
+
+bool checkString(const string& str, const string& chars) {
+    for (char c : chars) {
+        if (str.find(c) == string::npos) {
+            return false;
+        }
+    }
+    return true;
+}
+
+extern "C" __declspec(dllexport) bool checkWords(const char* sentence, const char* wordsToFind) {
+    string wordsToFindSet = wordsToFind;
+
+    if (!checkString(sentence, wordsToFindSet)) {
+        return false;
+    }
+
+    return true;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
+    switch (ul_reason_for_call) {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
---
+++ Praktos++/Praktos++.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+using namespace std;
+
+typedef bool (*checkWordsFunc)(const char*, const char*);
+
+int main() {
+    SetConsoleCP(1251);
+    setlocale(LC_ALL, "Russian");
+    HINSTANCE hGetProcIDDLL = LoadLibraryA("DLLMain.dll");
+
+    if (hGetProcIDDLL == nullptr) {
+        cout << "Не удалось загрузить динамическую библиотеку" << endl;
+        return EXIT_FAILURE;
+    }
+
+    checkWordsFunc checkString = (checkWordsFunc)GetProcAddress(hGetProcIDDLL, "checkWords");
+    if (!checkString) {
+        cout << "Функция не найдена" << endl;
+        FreeLibrary(hGetProcIDDLL);
+        return EXIT_FAILURE;
+    }
+
+    string sentence, wordsToFind;
+    cout << "Введите слово или предложение: ";
+    getline(cin, sentence);
+    cout << "Введите символы для поиска: ";
+    getline(cin, wordsToFind);
+
+
+    if (checkString(sentence.c_str(), wordsToFind.c_str())) {
+        cout << "Все символы <<" << wordsToFind << ">> найдены" << endl;
+    }
+    else {
+        cout << "Не все символы <<" << wordsToFind << ">> найдены" << endl;
+    }
+    FreeLibrary(hGetProcIDDLL);
+
+    return EXIT_SUCCESS;
+}
